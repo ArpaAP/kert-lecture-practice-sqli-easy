4adc1f4cd5b955d43d6f90727bdfaf6dc235378f
ArpaAP <arpa1705@gmail.com>
2025-10-02 17:23:31 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     setMessage('')
 
     try {
-      const response = await fetch('http://practice-sqli-easy.kert.space/api/login', {
+      const response = await fetch('https://practice-sqli-easy.kert.space/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
